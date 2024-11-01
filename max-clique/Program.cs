5bbb8adcf4c7b033e3656930340fdd9cab3d9ed0
MightyFilipns <filip.milosevicns@gmail.com>
2024-11-01 19:41:05 +0100

    Max clique

--- max-clique/Program.cs
+++ max-clique/Program.cs
@@ -8,40 +8,63 @@ namespace max_clique
     internal class Program
     {
         static bool[,] AdjMatrix = null;
+
+        private static int matrix_size = -1;
+
+        static int max_clique = 0;
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+
+            matrix_size = n;
             AdjMatrix = new bool[n,n];
             for (int i = 0; i < n; i++)
             {
-                var d = Console.ReadLine().Where(char.IsAsciiDigit).ToList().Select(a => a == '1').ToList();
-                for (var i1 = 0; i1 < d.Count(); i1++)
+                var d = Console.ReadLine().Split(" ");
+                for (var i1 = 0; i1 < n; i1++)
                 {
-                    AdjMatrix[i, i1] = d[i1];
-                }
-            } 
-
-            int best = -1;
-            for (int i = 0; i < n; i++)
-            {
-                int current = 0;
-                List<int> current_cliquies = new List<int>();
-                for (int j = 0; j < n; j++)
-                {
-                    if (current_cliquies.All(a => AdjMatrix[a, j] || j == a))
-                    {
-                        current_cliquies.Add(j);
-                    }
-                }
-
-                current = current_cliquies.Count();
-
-                if (current > best)
-                {
-                    best = current;
+                    AdjMatrix[i, i1] = d[i1] == "1";
                 }
             }
-            Console.WriteLine(best);
+            DateTime ds = DateTime.Now;
+
+            BronKerbosch([], Enumerable.Range(0, n).ToArray(), []);
+
+            DateTime de = DateTime.Now;
+            var rez = de - ds;
+            Console.WriteLine(rez.Milliseconds + " ms");
+
+
+            Console.WriteLine(max_clique);
+        }
+
+
+        public static void BronKerbosch(int[] R, int[] P, int[] X )
+        {
+            if (P.Length == X.Length && P.Length == 0)
+            {
+                max_clique = Math.Max(max_clique, R.Length);
+            }
+
+            foreach (var i in P.ToArray())
+            {
+                BronKerbosch(R.Union([i]).ToArray(),P.Intersect(N(i)).ToArray(),X.Intersect(N(i)).ToArray());
+                P = P.Except([i]).ToArray();
+                X = X.Union([i]).ToArray();
+            }
+
+            int[] N(int v)
+            {
+                List<int> result = new List<int>();
+                for (int i = 0; i < matrix_size; i++)
+                {
+                    if (AdjMatrix[v, i])
+                    {
+                        result.Add(i);
+                    }
+                }
+                return result.ToArray();
+            }
         }
     }
 }
