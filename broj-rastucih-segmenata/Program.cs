e813263d4069b5060c031c7dc9bcf9e4e78fffdf
Filip <filip.milosevicns@gmail.com>
2024-12-06 12:52:12 +0100

    Zadaci 06.12.20.24

---
+++ broj-rastucih-segmenata/Program.cs
@@ -0,0 +1,40 @@
+﻿using Console = System.Console;
+
+namespace broj_rastucih_segmenata;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<int> lista = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            lista.Add(int.Parse(Console.ReadLine()));
+        }
+        //------------------BROKEN------------------//
+        int cnt = 0;
+        int len = 0;
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                if (lista[j] <= lista[j-1])
+                {
+                    if (len != 0)
+                    {
+                        cnt++;
+                        len = 0;
+                        break;
+                    }   
+                }
+                else if (len != 0)
+                {
+                    cnt++;
+                }
+                len++;
+            }
+        }
+        Console.WriteLine(cnt);
+    }
+}
---
+++ pokretni-prozor/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace pokretni_prozor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = Console.ReadLine();
+        string str2 = Console.ReadLine();
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        str2.ToCharArray().ToList().ForEach(x =>
+        {
+            if(!dict.TryAdd(x, 0))
+                dict[x]++;
+        });
+        int cnt = 0;
+        Dictionary<char, int> dict2 = new Dictionary<char, int>(dict);
+        foreach (var t in str1)
+        {
+            if (dict.ContainsKey(t) && dict2[t] > 0)
+            {
+                dict[t]--;
+                if (dict.All(a => a.Value == 0))
+                {
+                    cnt++;
+                }
+            }
+            else
+            {
+                dict = new(dict2);
+            }
+        }
+        Console.WriteLine(cnt);
+    }
+}
